b4f26951437d4365bdd87d41ba4bfb9977ef6dbd
pandrey76 <pandrey76@yandex.ru>
2019-01-24 13:17:42 +0300

    Change to tabular way of displaying inline related objects!
    With that TabularInline (instead of StackedInline), the related objects are displayed in a more compact, table-based format

--- polls/admin.py
+++ polls/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
                     (None, {'fields': ['question_text']}),
